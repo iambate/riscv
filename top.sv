6274ce95aea769dfd13ec592c097ad58b6bb5814
Sagar Shah <schshah@cs.stonybrook.edu>
2017-02-01 18:01:42 -0500

    Read from DRAM(dramsim) using READ & MEMORY command

--- top.sv
+++ top.sv
@@ -24,13 +24,40 @@ module top
 );
 
   logic [63:0] pc;
-
+  logic [8:0] no_of_bytes;
   always @ (posedge clk)
     if (reset) begin
       pc <= entry;
+      no_of_bytes <= 'd8;
     end else begin
-      $display("Hello World!  @ %x", pc);
-      $finish;
+      if(bus_respcyc)
+      begin
+        if(!bus_resp)
+          $finish;
+        else begin
+          $display("Hello World!  @ %x", pc);
+          $display("bus_resp %h", bus_resp);
+          $display("bus_resptag %h", bus_resptag);
+          $display("");
+          pc <= pc + 8;
+          bus_req <= pc;
+          bus_respack <= bus_respcyc;
+          bus_reqcyc <= 0;
+          no_of_bytes <= no_of_bytes +1;
+        end
+      end
+
+      if(no_of_bytes == 'd8)
+      begin
+        bus_respack <= 0;
+        bus_reqcyc <= 1;
+        bus_req <= pc;
+        bus_reqtag <= `SYSBUS_READ<<12|`SYSBUS_MEMORY<<8;
+        $display("bus_reqtag %b", bus_reqtag);
+        no_of_bytes <= 0;
+      end else begin
+        bus_reqcyc <= 0;
+      end
     end
 
   initial begin
