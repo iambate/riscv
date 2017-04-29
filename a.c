f7e50ae4e869e0bbd8ac76cded6767fed0913e1e
Sagar Shah <schshah@cs.stonybrook.edu>
2017-04-29 12:05:05 -0400

    Add log and other files for debugging

--- a.c
+++ a.c
@@ -1,11 +1,14 @@
+#include <unistd.h>
 int main() {
 	int a[5], b[5], c[5];
 	for(int i=0;i<5;i++) {
-		a[i] = 0;
-		b[i] = 1;
+		a[i] = 10;
+		b[i] = 25;
 	}
 	for(int i=0;i<5;i++) {
 		c[i] = a[i] + b[i];
 	}
-	return c[4];
+//	printf("%d", c[i]);
+	write(1, "Sagar", 5);
+	return 0;
 }
